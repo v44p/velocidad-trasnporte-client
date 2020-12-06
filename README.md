f39a0927f958b43e25b2ecc2553ed89ce7b39f94
vlad <vlad@v44p.com>
2020-12-06 01:13:08 -0300

    first commit

--- README.md
+++ README.md
@@ -10,8 +10,6 @@ se debería buildear la imagen de docker y luego correr mediante
 docker run -it -d --rm \
     -e CLIENT_ID=<cliente id> \
     -e CLIENT_SECRET=<client secret> \
-    -e STORAGE_ACCOUNT_NAME=<nombre storage> \
-    -e STORAGE_ACCOUNT_KEY= <key account> \
     -e CONTAINER_NAME=<container name> \
     -p 8888:8888 \
     --name velocidad-client \
--- velocidad_trp/objects.py
+++ velocidad_trp/objects.py
@@ -1,5 +1,6 @@
 import os
 import json
+import requests
 
 from velocidad_trp.utils import http_transporte
 from datetime import datetime
@@ -13,141 +14,31 @@ class VelocidadTransporte:
     client_secret = ''
     name_file = ''
     path_local = ''
-
+    params = {
+        'client_id':"",
+        'client_secret':""
+    }
 
     def __init__(self, url_transporte:str, client_id:str, client_secret:str ):
         self.url = url_transporte
-        self.client_id = client_id
-        self.client_secret = client_secret
-        
+        self.params["client_id"] = client_id
+        self.params["client_secret"] = client_secret
 
     @property
     def get_json(self):
-        params = {
-            'client_id':self.client_id,
-            'client_secret':self.client_secret
-        }
         try:
-            print(params)
-            r = http_transporte(self.url, params)
-            print(r)
-            self.respuesta = r.json()
-            self.__get_name()
-            self.__write_json()
-            return True
+            r = requests.get(self.url, params=self.params, timeout=10)
+            if r.status_code != 200:
+                print(f'Error request %d' % (r.status_code))
+                return None
+            return r
         except:
             print('Error en Velocidad Transporte')
-            return False
-
-
-    def __get_name(self):
-        '''
-            Genera un nombre para el archivo en base a la fecha.
-            Este nombre será usado para guardar en el storage        
-        '''
-        date_request = datetime.now()
-        year = date_request.year
-        month = date_request.month
-        day = date_request.day
-        hour = date_request.hour
-        minute = date_request.minute
-        second = date_request.second
-        self.name_file = [year, month, day, hour, minute, second]
-        return True
-
+            return None
 
     def __write_json(self):
-        (hour, minute, second ) = self.name_file[3:]
-        self.path_local = f'vel_trp_%s_%s_%s.json' % (hour, minute, second)
-        try:
-            with open(self.path_local, 'w') as f:
-                json.dump(self.respuesta, f)
-        except:
-            print('no se pudo guardar')
-
-    @property   
-    def delete_json(self):
-        '''
-            Elimina el json en la vm
-        '''
-        try:
-            os.remove(self.path_local)
-            print(f'[INFO] delete s%' % self.path_local)
-        except:
-            print('No exise')
-
-
-
-
-class Storage:
-
-    response = ''
-    url_file = ''
-    container_name = ''
-
-    def __init__(self, container_name, account_name, account_key):
-        self.container_name = container_name
-        self.account_name = account_name
-        self.account_key = account_key
-
-
-    def connect_storage(self):
         pass
 
-
-        
-
-class DataStorage(Storage):
-    '''
-        Esta clase toma la libreria para conectarse a un blob storage,
-        de la misma forma podría crearse una clase para otro tipo de 
-        almacenamiento.
-    '''
-    
-    block_blob_service = ''
-    container_name = ''
-
-    
-    def connect_storage(self):
-        from azure.storage.blob import BlockBlobService, PublicAccess
-        print(self.account_name)
-        print(self.account_key)
-        try:
-            self.block_blob_service = BlockBlobService(
-                account_name=self.account_name,
-                account_key=self.account_key
-                )
-            #return block_blob_service
-        except:
-            print('ERROR' + ' No se pudo conectar')
-            #return False
-
-
-    def upload_blob(self, prefix_container, name_file):
-        path_full_container = 'input_api/'\
-                + '/'.join([str(s) for s in prefix_container[:3]])\
-                + '/' + name_file
-
-        print('[INFO] Subiendo archivo a container ...')
-        print('[INFO]', self.container_name)
-        print('[INFO]', path_full_container)
-        print('[INFO]', name_file) 
-        print()               
-        #try:
-        self.block_blob_service\
-            .create_blob_from_path(
-                self.container_name,
-                path_full_container,
-                name_file
-            )
-        #except:
-        #    print('No se pudo copiar al storage')
-
-
-
-
-class DataLake(Storage):
-    pass
 
 
 if __name__ == '__main__':
